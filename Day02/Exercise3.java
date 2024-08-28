99a3e1d15ac5646094979f56d0222c4f6e7f56de
Eric Manley <ericm@thirdcoastcommodities.com>
2024-08-28 10:11:54 -0500

    starter code for Exercise 3

---
+++ Day02/Exercise3.java
@@ -0,0 +1,12 @@
+public class Exercise3 
+{
+    public static void main(String[] args) 
+    {
+        String name;
+        name = "Manley, Eric";
+        System.out.println("The original string is "+name);
+
+        // write the code to convert the string to all lowercase and print it out
+
+    }
+}
