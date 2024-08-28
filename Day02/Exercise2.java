42829db14da9ab0dbe1954bb318c4bec7ec5c23d
Eric Manley <ericm@thirdcoastcommodities.com>
2024-08-28 10:03:58 -0500

    updated exercise

--- Day02/Exercise2.java
+++ Day02/Exercise2.java
@@ -3,8 +3,8 @@ public class Exercise2
     public static void main(String[] args) 
     {
         long price_of_gas_per_gallon = 3.54
-        milesHome  = 11
-        float miles_per_gallon = 45;
+        milesHome  = 26
+        float miles_per_gallon = 21;
 
         int cost = price_of_gas_per_gallon * milesHome / miles_per_gallon
         print("Driving", milesHome, "will cost", cost);
