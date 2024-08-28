7c46ef595e46f0c1528f62b87e9d298780af0e1c
Eric Manley <ericm@thirdcoastcommodities.com>
2024-08-28 11:32:00 -0500

    literals example

---
+++ Day02/Literals.java
@@ -0,0 +1,12 @@
+// This program has literals and a variable
+
+public class Literals 
+{
+    public static void main(String[] args)
+    {
+        int apples;
+
+        apples = 20;
+        System.out.println("I sold " + apples + " bags of apples.");
+    }
+}
