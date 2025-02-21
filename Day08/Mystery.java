9399d9352b69d9a96c4377a0bd6422854f32010c
andrei-migunov <andrei.migunov@drake.edu>
2025-02-21 09:32:40 -0600

    Update Mystery.java

--- Day08/Mystery.java
+++ Day08/Mystery.java
@@ -8,6 +8,11 @@ public class Mystery {
         numArray[0] = numArray[0]/2.0;
     }
 
+    public static void replaceArray(double[] numArray) {
+        numArray = new double[1];
+        numArray[0] = 3.14159;
+    }
+
     public static void swap(int a, int b) {
         int temp = a;
         a = b;
@@ -15,18 +20,23 @@ public class Mystery {
     }
 
     public static void main(String[] args) {
+        // int x = 7;
+        // int y = -23;
+        // swap(x,y);
+        // System.out.println("x is " + x + " and y is " + y);
+
         double data1 = 10.0;
         double[] data2 = new double[1];
         data2[0] = 10.0;
         cutInHalf(data1);
-        sliceInHalf(data2);
+        // sliceInHalf(data2);
+        replaceArray(data2);
         System.out.println(data1);
         System.out.println(data2[0]);
+        
 
-        //int x = 7;
-        //int y = -23;
-        //swap(x,y);
-        //System.out.println("x is " + x + " and y is " + y);
+        
+
     }
 
 }
