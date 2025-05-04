b37e8fa7d6e0d7952024e7cb84ce61e71cc078d0
andrei-migunov <andrei.migunov@drake.edu>
2025-05-04 09:39:29 -0500

    update main

--- Day28/Main.java
+++ Day28/Main.java
@@ -3,12 +3,9 @@ public class Main {
         Character mage = new Character("Elowen", 50, "Fire");
         Spell fireball = new Spell("Fireball", 30, "Fire");
 
-        Spell ice_shard = new Spell("Ice Shard", 10, "Ice");
-
         View view = new View();
         Controller controller = new Controller(mage, view);
 
         controller.handleCasting(fireball);
-        controller.handleCasting(ice_shard);
     }
 }
